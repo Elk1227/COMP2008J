d08540bac2c667589b2caa69ef117b83b0e37671
Elk1227 <yaohui.ye@ucdconnect.ie>
2023-04-20 19:54:51 +0800

    forth

--- src/Cards/ActionCard.java
+++ src/Cards/ActionCard.java
@@ -1,5 +1,10 @@
 package Cards;
 
-public class ActionCard {
+public abstract class ActionCard extends Card {
 
+    public ActionCard(String name, int value, boolean isUsed, boolean becomePriceCard) {
+        super(name, value, isUsed, becomePriceCard);
+    }
+
+    public abstract void function();
 }
