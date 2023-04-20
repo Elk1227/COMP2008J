0fa11cdacccc31b778472205254f2e65f529c28e
Elk1227 <yaohui.ye@ucdconnect.ie>
2023-04-20 18:15:53 +0800

    second

---
+++ src/Cards/Card.java
@@ -0,0 +1,15 @@
+package Cards;
+
+public abstract class Card {
+    final String name;
+    final int value;
+    boolean isUsed;
+    boolean becomePriceCard;
+
+    public Card(String name, int value, boolean isUsed, boolean becomePriceCard) {
+        this.name = name;
+        this.value = value;
+        this.isUsed = isUsed;
+        this.becomePriceCard = becomePriceCard;
+    }
+}
