988dcd3f66e87cc36c76c343dc5d7e85b85fefed
yeyaohui <yaohui.ye@ucdconnect.ie>
2023-04-20 14:56:13 +0800

    second commit

---
+++ COMP2008J/src/Cards/ActionCard/ActionCard.java
@@ -0,0 +1,12 @@
+package Cards.ActionCard;
+
+import Cards.Card;
+
+public abstract class ActionCard extends Card {
+
+    public ActionCard(String name, int value, boolean isUsed, boolean becomePriceCard) {
+        super(name, value, isUsed, becomePriceCard);
+    }
+
+    public abstract void function();
+}
---
+++ COMP2008J/src/Cards/PriceCard/PriceCard.java
@@ -0,0 +1,10 @@
+package Cards.PriceCard;
+
+import Cards.Card;
+
+public class PriceCard extends Card {
+    public PriceCard(String name, int value, boolean isUsed, boolean becomePriceCard) {
+        super(name, value, isUsed, becomePriceCard);
+    }
+}
+
