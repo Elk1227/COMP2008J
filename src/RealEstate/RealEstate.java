d0ac8c2d1043f31ed143797d1e4d6c6beef223b2
Elk1227 <yaohui.ye@ucdconnect.ie>
2023-05-15 20:11:14 +0800

    eight

---
+++ src/RealEstate/RealEstate.java
@@ -0,0 +1,13 @@
+package RealEstate;
+
+import Building.Building;
+import Player.Player;
+
+import java.util.ArrayList;
+
+public class RealEstate {
+    ArrayList<Building> buildings ;
+    Player owner;
+
+    public void addRealEstate(){}
+}
